146e4c8e18b380644c9abe3591870d69cd9c2975
Ty Jacalan <muzuuu@protonmail.com>
2025-12-08 19:22:09 +0800

    Edit building page styles
    Signed-off-by: Ty Jacalan <muzuuu@protonmail.com>

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="h-full min-w-[350px] container mx-auto flex flex-col items-center justify-between">
+    <main className="h-full min-h-screen min-w-[350px] container mx-auto flex flex-col items-center justify-center">
         <p>Building in progress...</p>
     </main>
   );
