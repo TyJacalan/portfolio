1c096c6b1e538a45e4dccf37d766adda6e2aa978
Ty Jacalan <muzuuu@protonmail.com>
2025-12-09 15:27:12 +0800

    Update color config to tailwind v4
    Signed-off-by: Ty Jacalan <muzuuu@protonmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,5 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "rgb(var(--primary))",
-        secondary: "rgb(var(--secondary))",
-        tertiary: "rgb(var(--tertiary))",
-        foreground: "rgb(var(--foreground))",
-        muted: "rgb(var(--muted))",
-        accent: "rgb(var(--accent))",
-      },
-    },
-  },
   plugins: [],
 };
